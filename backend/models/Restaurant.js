e3898a4b5467c9734a6e82f00927ca28877b2e14
Navya Lakshmi Manda <mandanavyalakshmi@gmail.com>
2026-01-28 08:24:15 +0530

    Update Restaurant.js

--- backend/models/Restaurant.js
+++ backend/models/Restaurant.js
@@ -3,13 +3,18 @@ import mongoose from "mongoose";
 const restaurantSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
+  },
+  name_hi: {
+    type: String,
+  },
+  name_te: {
+    type: String,
   },
   image: {
     type: String,
-    required: true
+    required: true,
   },
- 
 });
 
 export default mongoose.model("Restaurant", restaurantSchema);
